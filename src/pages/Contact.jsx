61dcb2053aa814b76dad608d8b1c18f39a2b8982
Mohamed Zoraa <mohamedzoraa100@gmail.com>
2026-01-04 15:38:18 +0100

    fix contatc page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,52 +1,68 @@
-import { doc, getDoc } from "firebase/firestore"
-import { useEffect, useState } from "react"
-import { useParams, useSearchParams } from "react-router-dom"
-import { db } from "../firebase.config"
-import { toast } from "react-toastify"
-
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
 
 function Contact() {
-    const [message,setMessage]=useState("")
-    const [landlord,setLandlord]=useState(null)
-    const [searchParams,setSearchParams]=useSearchParams()
+  const [message, setMessage] = useState("");
+  const [landlord, setLandlord] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const params=useParams()
+  const params = useParams();
 
-    useEffect(()=>{
-        const getLandlord=async()=>{
-            const docRef=doc(db,"users",params.landlordId)
-            const docSnap=await getDoc(docRef)
-            if(docSnap.exists()){
-                setLandlord(docSnap.data())
-            }else{
-                toast.error("could not get landlord data")
-            }
-        }
-        getLandlord()
-    },[params.landlordId])
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, "users", params.landlordId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data());
+      } else {
+        toast.error("could not get landlord data");
+      }
+    };
+    getLandlord();
+  }, [params.landlordId]);
   return (
     <div className="pageContainer">
-        <header>
-            <p className="pageHeader">Contact Landlord</p>
-        </header>
-        {landlord!==null && (
-            <main>
-                <div className="contactLandlord">
-                    <p className="landlordName"> contact {landlord?.name}</p>
-                </div>
-                <form className="messageForm">
-                    <div className="messageDiv">
-                        <label htmlFor="message" className="messageLabel">Message</label>
-                        <textarea name="message" id="message" className="textarea" value={message} onChange={(e)=>setMessage(e.target.value)} ></textarea>
-                    </div>
-                    <a href={`mailto:${landlord.email}?subject=${searchParams.get("listingName")}&body=${message}`}>
-                        <button type="button" className="primaryButton">Send Message</button>
-                    </a>
-                </form>
-            </main>
-        )}
+      <header>
+        <p className="pageHeader">Contact Landlord</p>
+      </header>
+      {landlord !== null && (
+        <main>
+          <div className="contactLandlord">
+            <p className="landlordName"> contact {landlord?.name}</p>
+          </div>
+          <form className="messageForm">
+            <div className="messageDiv">
+              <label htmlFor="message" className="messageLabel">
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="textarea"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
+            </div>
+            <a
+              className={`primaryButton ${!message ? "disabled" : ""}`}
+              href={
+                message
+                  ? `mailto:${landlord.email}?subject=${encodeURIComponent(
+                      searchParams.get("listingName")
+                    )}&body=${encodeURIComponent(message)}`
+                  : undefined
+              }
+            >
+              Send Message
+            </a>
+          </form>
+        </main>
+      )}
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
